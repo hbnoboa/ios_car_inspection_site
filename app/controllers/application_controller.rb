67751c605bde13c7278e35d656a01d727fa540d4
hbnoboa <hbnoboa11@gmail.com>
2024-02-09 08:59:05 -0300

    main

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_with_token!
-    before_action :authenticate_user!
+    before_action :authenticate_user_or_token!
+    before_action :authenticate_user!, unless: :token_authenticated?
   
     def after_sign_in_path_for(resource)
       # Personalize this as needed
@@ -9,18 +9,28 @@ class ApplicationController < ActionController::Base
   
     private
   
-    def authenticate_with_token!
-        authenticate_with_http_token do |token, options|
-        puts "started"
-        user = User.find_by(authentication_token: token)
-        @auth_token = token
-        puts user
-        if user
-          @current_user = user
-        else
-          false
+    def authenticate_user_or_token!
+        if current_user.nil?
+          authenticate_with_token!
         end
       end
+    
+    def authenticate_with_token!
+        puts "started"
+        authenticate_with_http_token do |token, _options|
+            user = User.find_by(authentication_token: token)
+            if user
+            puts user
+            @current_user = user
+            else
+            puts "error"
+            head :unauthorized
+            end
+        end
+    end
+
+    def token_authenticated?
+        !!@current_user
     end
   end
   
